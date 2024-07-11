28bff4abe355afb7c1585dbf8ffb1ff1b2edfd3f
admin <admin@admins-MacBook-Pro.local>
2024-07-11 10:07:47 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 function App() {
@@ -7,15 +7,14 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://americanlibertine.net"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+	  americanlibertine.net
         </a>
       </header>
     </div>
